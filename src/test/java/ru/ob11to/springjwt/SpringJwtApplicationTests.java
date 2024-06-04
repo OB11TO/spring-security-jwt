22d604b7a7b5a215bd9c6cf216355fe4daaf5731
Artem Dagridjan <adagridjan@alvioneurope.ru>
2024-06-04 16:41:33 +0300

    git init

---
+++ src/test/java/ru/ob11to/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ob11to.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
